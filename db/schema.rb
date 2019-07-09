6a86346e10c818d81480a7d598b975b343065359
Johanna Brewer <johannab@gmail.com>
2019-07-08 23:53:47 -0400

    Completely rebuilds repo without webpack which is a hot pile of trash that was making the application crawl to a standstill.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_203159) do
+ActiveRecord::Schema.define(version: 2019_07_09_031659) do
 
   create_table "pledges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name"
--- test/controllers/pledges_controller_test.rb
+++ test/controllers/pledges_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PledgeControllerTest < ActionDispatch::IntegrationTest
+class PledgesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
