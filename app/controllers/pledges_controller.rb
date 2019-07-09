3b8800915ea7c2f304f7786a11b647c7bda38107
Johanna Brewer <johannab@gmail.com>
2019-07-08 22:43:23 -0400

    Moves forcing of twitch usernames to be non-blank (that is the must either be nil, or actually have a value) to model level in case there exist some null strings in the original DB. This preps for batch import.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -5,7 +5,7 @@ class PledgesController < ApplicationController
   end
   
   def create
-    @pledge = Pledge.new(pledge_params.reject{|_, v| v.blank?})        
+    @pledge = Pledge.new(pledge_params)
         
     if @pledge.save
       flash[:notice] = "Successfully pledged"
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,6 +1,7 @@
 class Pledge < ApplicationRecord
   
   before_create :set_signed_on
+  before_save   :screen_twitch_username
   
   validates_presence_of    :first_name, :last_name, :email
   validates_uniqueness_of  :email, case_sensitive: false
@@ -11,6 +12,12 @@ class Pledge < ApplicationRecord
     def set_signed_on
       if !self.signed_on.present?
         self.signed_on = Time.now
+      end
+    end
+    
+    def screen_twitch_username
+      if twitch_username.blank?
+        self.twitch_username = nil
       end
     end
   
