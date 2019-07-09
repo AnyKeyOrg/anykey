47c7cac4dc969acc0bb401b59a1dddd4d21ce15b
Johanna Brewer <johannab@gmail.com>
2019-07-08 22:32:23 -0400

    Creates before create callback which ensures that signed_on is set for users who are signing in real time, yet still allows for that param to be passed in when we are going to be porting the data over in batches.

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,7 +1,17 @@
 class Pledge < ApplicationRecord
+  
+  before_create :set_signed_on
+  
   validates_presence_of    :first_name, :last_name, :email
   validates_uniqueness_of  :email, case_sensitive: false
   validates_format_of      :email, with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
 
+
+  private
+    def set_signed_on
+      if !self.signed_on.present?
+        self.signed_on = Time.now
+      end
+    end
   
 end
