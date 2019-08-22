cb39ccd0a1084a4d1cd1ccc3823927d93c0dd483
Johanna Brewer <johannab@gmail.com>
2019-08-21 21:16:59 -0400

    Sets up basic i18n for pledge page. Ensures error messages translation is working. Uses Italian as alt-test language.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base  
+  before_action :set_locale
+  
+  def set_locale
+    begin
+      I18n.locale = params[:locale] || I18n.default_locale
+    rescue I18n::InvalidLocale
+      I18n.locale = I18n.default_locale
+    end
+  end
+  
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
 end
