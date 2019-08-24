8f933ebee9cc7e14a253c3ccca1a3088b057f6f0
Johanna Brewer <johannab@gmail.com>
2019-08-24 01:34:03 -0400

    Adds todo.

--- app/assets/javascripts/background-resize.js
+++ app/assets/javascripts/background-resize.js
@@ -1,6 +1,8 @@
 /* Resizes full window image backgrounds to contain content as window scales */
 /* Could probably be much DRYer bu works for now */
 
+/* TODO: make sure this doesn't throw errors on pages without these elements */
+
 function resizeBackgrounds() {
   var glhfInnerHeight = document.getElementById('glhf-inner').clientHeight;
   document.getElementById('glhf-section').style.height = glhfInnerHeight+160+'px';
