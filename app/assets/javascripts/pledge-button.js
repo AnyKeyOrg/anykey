03461d9adcf88c0e3421ebb016c2f7051bab83aa
Johanna Brewer <johannab@gmail.com>
2019-08-26 15:06:00 -0400

    Makes tapestry a bit smaller and adds CTA to header which triggers scroll animation to display pledge form.

---
+++ app/assets/javascripts/pledge-button.js
@@ -0,0 +1,22 @@
+/* Smoothly scrolls to pledge form from header */
+
+function setupPledgeButton() {
+  $(".pledge-button").click(function(e) {
+    e.preventDefault();    
+    // document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+
+    var element = document.querySelector(this.getAttribute('href'));
+       var headerOffset = 20;
+       var elementPosition = element.getBoundingClientRect().top;
+       var offsetPosition = elementPosition - headerOffset;
+
+       window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+       });
+  });
+}
+
+$(document).ready(function() {
+  setupPledgeButton();
+});
