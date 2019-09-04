225f4d84e46e55452465f29634af0a6b6141e29e
Johanna Brewer <johannab@gmail.com>
2019-09-04 04:24:11 -0400

    Allow staff to view original screenshot uploaded to report.

---
+++ app/assets/javascripts/report-screenshots.js
@@ -0,0 +1,24 @@
+/*
+ * Shows and hides overlay for screenshot
+ * original image display on report review.
+ */
+
+function setupScreenshotOverlay() { 
+  $("#report-screenshot").mouseover(function(e) {
+    $("#screenshot-overlay").show();
+  });
+  
+  $("#report-screenshot").mouseout(function(e) {
+    $("#screenshot-overlay").hide();
+  });
+  
+  $("#report-screenshot").click(function(e) {
+    e.preventDefault();
+    window.open($(this).data('url'), "_blank");
+  });
+  
+}
+
+$(document).ready(function() {
+  setupScreenshotOverlay()
+});
