e0271e77fb3f69cd208ee8fe406ff32ea218aa0a
Johanna Brewer <johannab@gmail.com>
2019-09-05 04:41:16 -0400

    Creates front and backstage for affiliates.

--- app/controllers/affiliates_controller.rb
+++ app/controllers/affiliates_controller.rb
@@ -1,6 +1,39 @@
 class AffiliatesController < ApplicationController
   
+  layout "backstage"
+  
+  before_action :authenticate_user!,        only: [ :edit, :update ]
+  before_action :ensure_staff,              only: [ :edit, :update ]
+  before_action :find_affiliate,            only: [ :edit, :update ]
+  
   def index
+    if public_view?
+      render action: "public_index", layout: "application"
+    else
+      authenticate_user!
+      ensure_staff
+    end  
   end
+  
+  protected
+    def find_affiliate
+      @affiliate = Report.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to staff_index_path
+    end
+    
+    def public_view?
+      Rails.logger.info("*********")
+      Rails.logger.info(current_user == nil)
+
+      current_user == nil
+    end
+
+  private
+    def ensure_staff
+      unless current_user.is_moderator? || current_user.is_admin?
+        redirect_to root_url
+      end
+    end
   
 end
