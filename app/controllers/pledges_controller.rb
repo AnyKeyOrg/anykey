0b45132c8c4ffb66ecf87bef937a02d7b7270bec
Johanna Brewer <johannab@gmail.com>
2019-09-06 02:38:50 -0400

    Back to localhost for twitch callbacks and set reminder to rollover. Should be done with env variables.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -61,8 +61,9 @@ class PledgesController < ApplicationController
         end
         
         # Request an access token from Twitch
-        # TODO: change redirect URL to anykey.org  
-        response = HTTParty.post(URI.escape("#{ENV['TWITCH_API_BASE_URL']}/oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&code=#{params[:code]}&grant_type=authorization_code&redirect_uri=https://anykey.herokuapp.com/pledge"))
+        # TODO: change redirect URL to anykey.org
+        # Also in twitch_auth partial
+        response = HTTParty.post(URI.escape("#{ENV['TWITCH_API_BASE_URL']}/oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&code=#{params[:code]}&grant_type=authorization_code&redirect_uri=http://localhost:9292/pledge"))
 
         # Use token to view Twitch credentials and store for validation
         if response["access_token"].present?
