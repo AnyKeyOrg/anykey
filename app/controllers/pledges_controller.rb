1bea60678ece909a4d2ea497fa4fb426907f23a9
Johanna Brewer <johannab@gmail.com>
2019-09-09 04:18:05 -0400

    Handles case when a new pledger tries to auth with a twitch user who is already in our DB. Routes to a new show page with the badge equip instructions, keeps the pledge, but does not add the auth to it. Also ensures that all the contact us links in the pledge auth flow now point to the contact page (which is currently blank LOL).

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -79,25 +79,26 @@ class PledgesController < ApplicationController
           twitch_user = HTTParty.get(URI.escape("#{ENV['TWITCH_API_BASE_URL']}/user"), headers: {Accept: 'application/vnd.twitchtv.v5+json', Authorization: "OAuth #{response['access_token']}", "Client-ID": ENV['TWITCH_CLIENT_ID']})
                     
           if twitch_user.present?
-            #TODO: check if this Twitch user has already authed, and... do something if they have
-            #TODO: consider the case were a user who has authed and had their badge revoked comes back under a new email
-            #TODO: consider the case were a user has honestly authed under another email and comes back under a new email
-            #TODO: probably just dump all dupe-auths to an error page saying that twitch id exists, contact us for help
-            #TODO: or... if badge hasn't been revoked, move auth from old pledge email to new one?
+
+            # Check if Twitch user already pledged
+            if Pledge.find_by(twitch_id: twitch_user["_id"])
+              redirect_to pledge_path(@pledge, params: {status: 'duplicate'})
             #TODO: conclusion: dump all dupe-auths to an error page saying that twitch id exists, contact us for help
             
-            @pledge.twitch_id           = twitch_user["_id"]
-            @pledge.twitch_display_name = twitch_user["display_name"]
-            @pledge.twitch_email        = twitch_user["email"]
-            
-            #TODO: set pledge badge here 
-            #TODO: set twitch_authed_on here 
-
-            if @pledge.save
-              redirect_to pledge_path(@pledge)
             else
-              #TODO: catch failure and send to an error page
-            end
+              @pledge.twitch_id           = twitch_user["_id"]
+              @pledge.twitch_display_name = twitch_user["display_name"]
+              @pledge.twitch_email        = twitch_user["email"]
+            
+              #TODO: set pledge badge here 
+              #TODO: set twitch_authed_on here 
+
+              if @pledge.save
+                redirect_to pledge_path(@pledge)
+              else
+                #TODO: catch failure and send to an error page
+              end
+            end  
             
           else
             #TODO: catch failure and send to an error page
