d0415d083260bc037bcaee08dcfc2c0422bdae6e
Johanna Brewer <johannab@gmail.com>
2019-09-12 13:05:23 -0400

    Integrating Google Analytics.

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -21,5 +21,22 @@ html = <<-HTML
 HTML
     html.html_safe if flash[:notice]
   end
+  
+  def analytics
+    if Rails.env.production?
+script = <<-SCRIPT
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74421707-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-74421707-2');
+</script>
+SCRIPT
+      script.html_safe
+    end
+  end
     
 end
