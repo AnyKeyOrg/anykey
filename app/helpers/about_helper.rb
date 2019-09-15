5df12218b0c30ed4321d1646b742179d15778439
Johanna Brewer <johannab@gmail.com>
2019-09-15 09:33:51 -0400

    Adds metatags for secondary public-facing pages (reports, about, contact, data policy). Still no social share images.

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,2 +1,13 @@
 module AboutHelper
+  
+  def about_metatags
+    if controller.action_name == 'index'
+      render(partial: 'about/metatags_index')
+    elsif controller.action_name == 'contact'
+      render(partial: 'about/metatags_contact')
+    elsif controller.action_name == 'data_policy'
+      render(partial: 'about/metatags_data_policy')
+    end  
+  end
+  
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -8,4 +8,8 @@ module ReportsHelper
     end
   end
   
+  def reports_metatags
+    render(partial: 'reports/metatags')
+  end
+  
 end
