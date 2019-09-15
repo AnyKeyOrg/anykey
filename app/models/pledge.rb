e38f5b9c1e94a22963fb9d7535385a05f986a17b
Johanna Brewer <johannab@gmail.com>
2019-09-15 11:11:52 -0400

    Temporarily disable email uniqueness validation for old db import.

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -5,7 +5,8 @@ class Pledge < ApplicationRecord
   validates_presence_of    :first_name,
                            :last_name,
                            :email
-  validates_uniqueness_of  :email,
+  # TODO: enable after data import
+  #validates_uniqueness_of  :email,
                            case_sensitive: false
   validates_format_of      :email,
                            with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
