3aa67158793395b669fcf3421f20441d3083010f
Johanna Brewer <johannab@gmail.com>
2019-09-17 15:53:37 -0400

    Sets correct mailer domain for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   
   # Required default URL options for the Devise mailer
-  # TODO: set correct mailer domain for production
-  config.action_mailer.default_url_options = { host: 'anykey.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'anykey.com' }
   config.action_mailer.delivery_method = :smtp
   
   # Ignore bad email addresses and do not raise email delivery errors.
