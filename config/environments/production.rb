873ddf3a9e5c01fd1439dfcc41389d39c8cbd379
Johanna Brewer <johannab@gmail.com>
2019-09-18 11:34:25 -0400

    Another anykey.com, this time in the mailer. A user pointed it out. Yay.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   
   # Required default URL options for the Devise mailer
-  config.action_mailer.default_url_options = { host: 'anykey.com' }
+  config.action_mailer.default_url_options = { host: 'anykey.org' }
   config.action_mailer.delivery_method = :smtp
   
   # Ignore bad email addresses and do not raise email delivery errors.
