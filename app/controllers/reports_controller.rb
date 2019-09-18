fef6a32eac55ce4e35f7c2c4e5a96dbdcafbabbd
Johanna Brewer <johannab@gmail.com>
2019-09-18 09:35:13 -0400

    Okay the nil checked worked. What about blank. Let's try. Ruby I am bad at you.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -87,7 +87,7 @@ class ReportsController < ApplicationController
 
       if response.nil?
         @reported_twitch_user = nil
-      elsif response["users"].nil?
+      elsif response["users"].blank?
        @reported_twitch_user = nil
       else
         @reported_twitch_user = response["users"][0]["_id"]
