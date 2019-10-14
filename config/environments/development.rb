4be7ee1ee01db10b8e2d2dc9de7a7d9f64a81374
Johanna Brewer <johannab@gmail.com>
2019-10-14 06:55:34 -0400

    Uh... oops delivery was on in dev?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,5 +71,7 @@ Rails.application.configure do
   # config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :smtp
   
+  config.action_mailer.perform_deliveries = false
+  
   
 end
