4a54003606881deb7c9c029097d269213dc22992
Johanna Brewer <johannab@gmail.com>
2020-09-03 13:18:36 -0400

    Creates metatags for inclusion 101 and updates all metatags to have a space in live streaming.

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -3,6 +3,8 @@ module ResourcesHelper
   def resources_metatags
     if controller.action_name == 'index'
       render(partial: 'resources/metatags')
+    elsif controller.action_name == 'inclusion_101'
+      render(partial: 'resources/metatags_inclusion_101')   
     elsif controller.action_name == 'keystone_code'
       render(partial: 'resources/metatags_keystone_code')
     end  
