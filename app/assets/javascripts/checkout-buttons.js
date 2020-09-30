e4e0131a94ddae095615c3473ddcc1a182dec125
Johanna Brewer <johannab@gmail.com>
2020-09-30 15:11:16 -0400

    Updates Donation flow to use live Stripe products in prep for push to production.

--- app/assets/javascripts/checkout-buttons.js
+++ app/assets/javascripts/checkout-buttons.js
@@ -4,79 +4,79 @@
  */
 
 $(document).ready(function() {
-  var stripe = Stripe('pk_test_51HV8FHBQehflyhkgtPssoP5TEbKCTA0kKBSCFQQ7CAZRNSbGa0hfEKGwnoch5W4l7kRx9jZAimVwYc3xjccQg5az00YJHpPBvx');
+  var stripe = Stripe('pk_live_51HV8FHBQehflyhkgxv9mJo62lHAuTHiraarOvNo2h6cy9doKwLXHLqqFVk6gWYI0EZFOHVyFgo2yAZ9PL07n7ALh00XxiCdlG7');
 
-  var checkoutButton1 = document.getElementById('checkout-button-price_1HX8O0BQehflyhkgEfBifer2');
+  var checkoutButton1 = document.getElementById('checkout-button-price_1HXAc8BQehflyhkgOufcv8SR');
   if (checkoutButton1)
     checkoutButton1.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8O0BQehflyhkgEfBifer2', quantity: 1}],
+        lineItems: [{price: 'price_1HXAc8BQehflyhkgOufcv8SR', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton5 = document.getElementById('checkout-button-price_1HX7XRBQehflyhkgIejDYiKd');
+  var checkoutButton5 = document.getElementById('checkout-button-price_1HXAcFBQehflyhkgnMg71AhB');
   if (checkoutButton5)
     checkoutButton5.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX7XRBQehflyhkgIejDYiKd', quantity: 1}],
+        lineItems: [{price: 'price_1HXAcFBQehflyhkgnMg71AhB', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton10 = document.getElementById('checkout-button-price_1HX8UPBQehflyhkgOzOXJVu1');
+  var checkoutButton10 = document.getElementById('checkout-button-price_1HXAcMBQehflyhkgjycShX8z');
   if (checkoutButton10)
     checkoutButton10.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8UPBQehflyhkgOzOXJVu1', quantity: 1}],
+        lineItems: [{price: 'price_1HXAcMBQehflyhkgjycShX8z', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton25 = document.getElementById('checkout-button-price_1HX8V8BQehflyhkgv5j8DBf6');
+  var checkoutButton25 = document.getElementById('checkout-button-price_1HXAcSBQehflyhkg7AQ7M90I');
   if (checkoutButton25)
     checkoutButton25.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8V8BQehflyhkgv5j8DBf6', quantity: 1}],
+        lineItems: [{price: 'price_1HXAcSBQehflyhkg7AQ7M90I', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton50 = document.getElementById('checkout-button-price_1HX8VlBQehflyhkgZtVDCFW0');
+  var checkoutButton50 = document.getElementById('checkout-button-price_1HXAcYBQehflyhkgad1lX1fs');
   if (checkoutButton50)
     checkoutButton50.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8VlBQehflyhkgZtVDCFW0', quantity: 1}],
+        lineItems: [{price: 'price_1HXAcYBQehflyhkgad1lX1fs', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton100 = document.getElementById('checkout-button-price_1HX8WLBQehflyhkgcGy10Lsd');
+  var checkoutButton100 = document.getElementById('checkout-button-price_1HXAcdBQehflyhkgvQF3bG9e');
   if (checkoutButton100)
     checkoutButton100.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8WLBQehflyhkgcGy10Lsd', quantity: 1}],
+        lineItems: [{price: 'price_1HXAcdBQehflyhkgvQF3bG9e', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
       })
     });
 
-  var checkoutButton500 = document.getElementById('checkout-button-price_1HX8WzBQehflyhkgk5u79eb4');
+  var checkoutButton500 = document.getElementById('checkout-button-price_1HXAchBQehflyhkglDAHzzPI');
   if (checkoutButton500)
     checkoutButton500.addEventListener('click', function () {
       stripe.redirectToCheckout({
-        lineItems: [{price: 'price_1HX8WzBQehflyhkgk5u79eb4', quantity: 1}],
+        lineItems: [{price: 'price_1HXAchBQehflyhkglDAHzzPI', quantity: 1}],
         mode: 'payment',
         successUrl: 'https://anykey.org/donate/success',
         cancelUrl: 'https://anykey.org/donate',
