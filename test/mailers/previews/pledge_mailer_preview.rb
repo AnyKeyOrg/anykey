8cd42a0e5ad101fc9c66b6c503b8d3e5d8adba63
Johanna Brewer <johannab@gmail.com>
2020-12-01 21:43:24 -0500

    Adds previews for currently non-existent html pledge emails.

--- test/mailers/previews/pledge_mailer_preview.rb
+++ test/mailers/previews/pledge_mailer_preview.rb
@@ -1,4 +1,26 @@
 # Preview all emails at http://localhost:3000/rails/mailers/pledge_mailer
 class PledgeMailerPreview < ActionMailer::Preview
-
+  def welcome_pledger
+    PledgeMailer.welcome_pledger(Pledge.first)
+  end
+    
+  def send_pledger_referral
+    PledgeMailer.send_pledger_referral(Pledge.first)
+  end
+    
+  def warn_pledger
+    PledgeMailer.warn_pledger(ConductWarning.first)
+  end
+    
+  def revoke_pledger
+    PledgeMailer.revoke_pledger(Revocation.first)
+  end
+    
+  def notify_reporter_warning
+    PledgeMailer.notify_reporter_warning(ConductWarning.first)
+  end
+    
+  def notify_reporter_revocation
+    PledgeMailer.notify_reporter_revocation(Revocation.first)
+  end
 end
