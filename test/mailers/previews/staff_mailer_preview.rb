bde375353b0ebc2189da2fd59293e85247cc01c0
Johanna Brewer <johannab@gmail.com>
2020-12-01 20:57:58 -0500

    Setups action mailer preview for html staff emails.

--- test/mailers/previews/staff_mailer_preview.rb
+++ test/mailers/previews/staff_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/staff_mailer
 class StaffMailerPreview < ActionMailer::Preview
-
+  def notify_staff_new_report
+    StaffMailer.notify_staff_new_report(Report.first)
+  end
+   
+  def send_weekly_stats_update
+    StaffMailer.send_weekly_stats_update(Time.now.beginning_of_day-7.days, Time.now.beginning_of_day)
+  end 
 end
