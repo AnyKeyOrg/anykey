46b294bb8c2ca92d8185cad055f776a514d27fd5
wninohira <wakananinohira@gmail.com>
2022-07-21 17:07:06 -0400

    Send confirmation email to reporter when a report is submitted

--- app/mailers/pledge_mailer.rb
+++ app/mailers/pledge_mailer.rb
@@ -1,35 +1,40 @@
 class PledgeMailer < ApplicationMailer
-  
+
   default from: DO_NOT_REPLY
 
   def welcome_pledger(pledge)
     @pledge = pledge
     mail(to: pledge.email, subject: "Thank you for taking the GLHF pledge!")
   end
-  
+
   def send_pledger_referral(pledge)
     @pledge = pledge
     mail(to: pledge.email, subject: "Your GLHF pledge referral link")
   end
-  
+
   def warn_pledger(warning)
     @warning = warning
     mail(to: warning.pledge.email, subject: "Warning about your conduct")
   end
-  
+
   def revoke_pledger(revocation)
     @revocation = revocation
     mail(to: revocation.pledge.email, subject: "Your AnyKey badge has been revoked")
   end
-  
+
   def notify_reporter_warning(warning)
     @warning = warning
     mail(to: warning.report.reporter_email, subject: "We've acted on your report")
   end
-  
+
   def notify_reporter_revocation(revocation)
     @revocation = revocation
     mail(to: revocation.report.reporter_email, subject: "We've acted on your report")
   end
-  
+
+  def confirm_reporter(report)
+    @report = report
+    mail(to: report.reporter_email, subject: "Badge abuse report submission confirmed")
+  end
+
 end
