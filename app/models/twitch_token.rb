e0c9f81ac740cff3d05727e59217a5668134db61
Johanna Brewer <johannab@gmail.com>
2023-02-15 18:46:00 -0500

    Fixes flaw in twitch ouath token refresh function which didn't account for the development case when a token is past expiration, not almost expiring, and needs to be refreshed.

--- app/models/twitch_token.rb
+++ app/models/twitch_token.rb
@@ -2,8 +2,8 @@ class TwitchToken < ApplicationRecord
   validates_presence_of :access_token, :expires_in
   
   def valid_token!
-    # If token is set to expire in less than a day, request a new one
-    if self.updated_at + self.expires_in > Time.now+1.day
+    # If token is set to expire in less than a day, or if it is expired, request a new one
+    if self.updated_at + self.expires_in > Time.now+1.day || self.updated_at < Time.now
       response = HTTParty.post(URI.escape("#{ENV['TWITCH_AUTH_BASE_URL']}/oauth2/token?client_id=#{ENV['TWITCH_CLIENT_ID']}&client_secret=#{ENV['TWITCH_CLIENT_SECRET']}&grant_type=client_credentials"))
 
       # Verify that a valid token has been received before setting it
