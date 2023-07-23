faa41409daecef5e12f215a01e95476cbc5292f2
Johanna Brewer <johannab@gmail.com>
2023-07-23 00:55:25 -0400

    Updates to Ruby version 3.1.2 (because latest 3.2.2 seems to require OpenSSL3 and I'm worried about that). Replaces deprecated URI.encode with CGI.parse in the Dragonfly middleware code that is destined for deletion. Surprised this all went smoothly. Not sure if I should update to Rails 7 or leave things be.

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -3,8 +3,8 @@ middleware = Anykey::Application.config.middleware
 # Sets up Dragonfly as middleware, but removes caching from environments where it's redundant
 middleware.insert 0, Rack::Cache, {
   :verbose     => true,
-  :metastore   => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/meta"),
-  :entitystore => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/body")
+  :metastore   => CGI.escape("file:#{Rails.root}/tmp/dragonfly/cache/meta"),
+  :entitystore => CGI.escape("file:#{Rails.root}/tmp/dragonfly/cache/body")
 }
 
 middleware.insert_after Rack::Cache, Dragonfly::Middleware, :images
