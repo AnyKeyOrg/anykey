76b6ff1ac55dd8027e69a88d625d7d14d1a3ed29
Johanna Brewer <johannab@gmail.com>
2023-07-24 05:31:08 -0400

    Dragonfly is a very deprecated hot mess. Add encode to the URI monkeypatch, should only affect Dragonfly since I update the rest of our code to stop using it already.

--- config/initializers/uri_monkeypatch.rb
+++ config/initializers/uri_monkeypatch.rb
@@ -4,7 +4,15 @@
 require 'uri'
 
 module URI
+  def self.escape(*args)
+    URI.encode_www_form_component(*args)
+  end
+  
   def self.unescape(*args)
     URI.decode_www_form_component(*args)
+  end
+  
+  def self.encode(*args)
+    URI.encode_www_form_component(*args)
   end
 end
