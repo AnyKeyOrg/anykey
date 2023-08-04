85aed3e0943ae3d944ba261764f2051101106caa
Johanna Brewer <johannab@gmail.com>
2023-08-04 16:49:29 -0400

    Ensures overlays for viewing full screenshots on concerns pages works. Very elegant bit of JQuery to do it. Not sure why I never figure that out before! Also improves overall display of screenshots.

--- app/assets/javascripts/concern-overlays.js
+++ app/assets/javascripts/concern-overlays.js
@@ -1,31 +1,18 @@
 /*
  * Shows and hides overlays for opening
- * original assets on concern review.
+ * original assets on concern review
+ * in a super DRY way! Recycle this!
  */
 
 function setupConcernOverlays() {
-  var concern_screenshots = $('#concern-screenshots');
-  if (concern_screenshots) {
-    for (var i=0; i < concern_screenshots.data('count'); i++){
-      console.log("#concern-screenshot-"+i);
-      $("#concern-screenshot-"+i).mouseover(function(e) {
-        console.log("mousedover");
-        console.log("#concern-screenshot-overlay-"+i);
-        $("#concern-screenshot-overlay-"+i).show();
-      });
-
-      $("#concern-screenshot-"+i).mouseout(function(e) {
-        console.log("mousedout");
-        console.log("#concern-screenshot-overlay-"+i);
-        $("#concern-screenshot-overlay-"+i).hide();
-      });
-
-      $("#concern-screenshot-"+i).click(function(e) {
-        e.preventDefault();
-        window.open($(this).data('url'), "_blank");
-      });
-    }
-  }
+  $(".concern-screenshot").on("mouseover mouseout", function() {
+    $(this).find(".concern-screenshot-overlay").toggle();
+  });
+  
+  $(".concern-screenshot").click(function(e) {
+    e.preventDefault();
+    window.open($(this).data('url'), "_blank");
+  });
 }
 
 $(document).ready(function() {
