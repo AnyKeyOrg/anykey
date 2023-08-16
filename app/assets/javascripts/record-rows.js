90c379714a7d147536d83a3d1b8a8cd138cf08f8
Johanna Brewer <johannab@gmail.com>
2023-08-16 12:41:10 -0400

    Gets command and control right ;]

--- app/assets/javascripts/record-rows.js
+++ app/assets/javascripts/record-rows.js
@@ -1,16 +1,14 @@
 // Handles clicks on record rows in staff dashboard
-// And after four years, finally enables right/context click!
+// And after four years, finally enables Cmd/Ctrl click!
 
 $(document).ready(function() {
   $(".record-row").click(function(e) {
     e.preventDefault();
-    if ($(this).data('url'))
-      window.location = $(this).data('url');
-  });
-  
-  $(".record-row").on("contextmenu", function(e) {
-    e.preventDefault();
-    if ($(this).data('url'))
-      window.open($(this).data('url'), '_blank');
+    if ($(this).data('url')) {
+      if (e.metaKey || e.ctrlKey)
+        window.open($(this).data('url'), '_blank');
+      else
+        window.location = $(this).data('url');
+    }
   });
 });
