e47590c33bf0f8a21f15cde9fabce9967533199a
Johanna Brewer <johannab@gmail.com>
2023-08-16 12:32:03 -0400

    Finally adds right click for record rows on staff dashboard. Wut? Pew pew\!

--- app/assets/javascripts/record-rows.js
+++ app/assets/javascripts/record-rows.js
@@ -1,12 +1,16 @@
-/*
- * Handles clicks on record rows in
- * staff dashboard.
- */
+// Handles clicks on record rows in staff dashboard
+// And after four years, finally enables right/context click!
 
 $(document).ready(function() {
   $(".record-row").click(function(e) {
     e.preventDefault();
     if ($(this).data('url'))
       window.location = $(this).data('url');
+  });
+  
+  $(".record-row").on("contextmenu", function(e) {
+    e.preventDefault();
+    if ($(this).data('url'))
+      window.open($(this).data('url'), '_blank');
   });
 });
