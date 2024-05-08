2e124220ad169c7704f1d65c5ea548de87249d5e
Paola Calle <pcalle@smith.edu>
2024-05-08 11:02:07 -0400

    re-organized and made expriy an optional param

--- app/controllers/concerns/rate_limitable.rb
+++ app/controllers/concerns/rate_limitable.rb
@@ -1,18 +1,19 @@
 module RateLimitable
     private 
 
-    def limit_create_request(base_key, redirection_path, limit = 9)
-        client_ip = request.remote_ip
+    CACHE_KEY_PATTERN = '%{base}_rate_limit:%{ip}'.freeze
 
-        rate_limit_key = "#{base_key}_rate_limit:#{client_ip}"
+    def limit_create_request(base_key, redirection_path, limit = 9, expiry: 60)
+        client_ip = request.remote_ip
+        rate_limit_key = CACHE_KEY_PATTERN % { base: base_key, ip: client_ip }
         rate_limit_count = Rails.cache.read(rate_limit_key).to_i
     
         if rate_limit_count >= limit
-          flash[:alert] = "Rate limit exceeded. Please try again later."
+          flash[:alert] = 'Rate limit exceeded. Please try again later.'
           redirect_to redirection_path
         end
   
-        Rails.cache.write(rate_limit_key, rate_limit_count + 1, expires_in: 60.seconds)
+        Rails.cache.write(rate_limit_key, rate_limit_count + 1, expires_in: expiry.seconds)
     end
 
 end
