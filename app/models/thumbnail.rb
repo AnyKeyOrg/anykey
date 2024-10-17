f738910f9252241b66244e205599228087073e68
Johanna Brewer <johannab@gmail.com>
2024-10-17 17:58:34 -0400

    That was also insufficient. Back to square one.

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -6,7 +6,7 @@ class Thumbnail
   attr_accessor :id, :uid
   class_attribute :redis
 
-  self.redis ||= Redis::Namespace.new('thumbnails', :redis => $redis, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  self.redis ||= Redis::Namespace.new('thumbnails', :redis => $redis)
 
   def self.find(id)
     if uid = redis.get(id)
